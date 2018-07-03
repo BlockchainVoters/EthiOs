b966e8eaa0ed636c1038244197e2f02d7bc5a6b7
Isaías <isaiahlima18@gmail.com>
2018-07-03 20:53:45 -0300

    Consegui acessar a conta. Talvez tenha de montar as transações na raça para serem enviadas

--- EthiOs/ViewController.swift
+++ EthiOs/ViewController.swift
@@ -7,19 +7,78 @@
 //
 
 import UIKit
+import EthereumKit
+import CryptoSwift
+import CryptoEthereumSwift
+
+let pass = "qwerty1234567890"
+let key = "3NvOsJAFQ0UFPFcBafxe"
+let node = "https://ropsten.infura.io/" + key
+let etherscan = "VCY6JC3G77VCFWYD7TMJW293W4KK9TT54R"
 
 class ViewController: UIViewController {
 
+    var address: String!
+    var privKey: String!
+    var publKey: String!
+
+    var config: Configuration!
+    var geth: Geth!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.config = Configuration(network: .ropsten, nodeEndpoint: node, etherscanAPIKey: etherscan, debugPrints: true)
+        self.geth = Geth(configuration: self.config)
+
+        self.createWallet()
+
+        self.getBalance()
+
+        
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func createWallet() {
+        do {
+            let defaults = UserDefaults.standard
+            if let account = defaults.object(forKey: "account") as? [String : String] {
+
+                self.address = account["addrss"]
+                self.privKey = account["privat"]
+
+            } else {
+
+                let mnemonic = Mnemonic.create(strength: .hight, language: .english)
+                let seed = try Mnemonic.createSeed(mnemonic: mnemonic, withPassphrase: pass)
+                let wallet = try Wallet(seed: seed, network: .ropsten, debugPrints: true)
+                let address = wallet.generateAddress()
+                let privkey = wallet.dumpPrivateKey()
+
+                let acc = ["addrss" : address,
+                           "privat" : privkey]
+
+                defaults.set(acc, forKey: "account")
+                defaults.synchronize()
+
+                self.address = address
+                self.privKey = privkey
+            }
+        } catch {
+
+        }
     }
 
+    func getBalance() {
+        self.geth.getBalance(of: self.address, blockParameter: .latest) { (result) in
+            switch result {
+            case .success(let balance):
+                print(#function, balance.wei)
+            case .failure(let error):
+                print(#function, error.localizedDescription)
+            }
+        }
+    }
 
 }
 
